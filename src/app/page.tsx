22f389af87c24f8994b8c55eab89810dd5afe83e
Ly Tran Hoang Hieu <lthhieu12.14lvl@gmail.com>
2023-12-02 16:56:24 +0700

    useFormState hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,31 @@
+'use client'
 import { SubmitButton } from "@/components/login/login.button"
+import { handleLogin } from "./actions"
+import { useFormState } from 'react-dom'
+import { useEffect } from "react"
+import { message } from 'antd';
 
 export default function Home() {
-  const handleLogin = async (formData: FormData) => {
-    'use server'
-    console.log('username', formData.get('username'), 'password', formData.get('password'))
-    await new Promise(resolve => setTimeout(resolve, 5000))
-    // mutate data
-    // revalidate cache
-  }
+  const [state, formAction] = useFormState(handleLogin, {})
+  useEffect(() => {
+    if (state?.data?.access_token) {
+      message.success('Login successfully')
+    } else {
+      message.error('Login fail')
+    }
+  }, [state])
   return (
     <div>
       <h2>HTML Forms</h2>
 
-      <form action={handleLogin}>
+      <form action={formAction}>
         <label>username:</label><br />
         <input type="text" name="username" /><br />
         <label >password:</label><br />
         <input type="text" name="password" /><br /><br />
         <SubmitButton />
       </form>
+      {JSON.stringify(state)}
     </div>
   )
 }
